110a47a15ef008f2e858009749f4a20a6aafa8fd
Martin Schaef <schaef@amazon.com>
2021-05-12 10:24:13 -0400

    now try with the real thing

--- createScorecard.sh
+++ createScorecard.sh
@@ -3,6 +3,7 @@ mkdir -p owasp-benchmark/results
 
 jq -rs 'reduce .[] as $item ({}; . * $item)' $(find . -type f -name "codeguru-results.json" )  > owasp-benchmark/results/combined.json
 
+rm owasp-benchmark/results/Benchmark_1.2-*.xml # remove findings of other tools
 cd owasp-benchmark
 mvn clean compile
 ./createScorecards.sh
